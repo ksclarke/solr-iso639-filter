cdc6a72eed199ff6410b534ac9664f8609e570dd
Kevin S. Clarke <ksclarke@gmail.com>
2013-08-20 21:44:19 -0400

    added logging to filter test

--- src/test/java/info/freelibrary/solr/ISO639ConversionFilterTest.java
+++ src/test/java/info/freelibrary/solr/ISO639ConversionFilterTest.java
@@ -1,20 +1,37 @@
 
 package info.freelibrary.solr;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import java.io.StringReader;
 
 import org.apache.lucene.analysis.MockTokenizer;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.BaseTokenStreamTestCase;
 
-import org.junit.Test;
-
 public class ISO639ConversionFilterTest extends BaseTokenStreamTestCase {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ISO639ConversionFilterTest.class);
 
     static {
         ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
     }
-    
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        String scope = System.getProperty("solr.scope");
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Running {} test using Solr '{}'",
+                    ISO639ConversionFilterTest.class.getSimpleName(), scope);
+        }
+    }
+
     @Test
     public void test() throws Exception {
         assertConvertsTo("en", new String[] {"English"});
