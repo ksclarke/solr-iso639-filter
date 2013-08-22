e0a3e6434d93bf4d2c0a4c6ba3242934255a8d58
Kevin S. Clarke <ksclarke@gmail.com>
2013-08-22 01:00:03 -0400

    fixed integration test's xmlpage classcastexception

--- src/test/java/info/freelibrary/solr/ISO639SolrIntegrationTest.java
+++ src/test/java/info/freelibrary/solr/ISO639SolrIntegrationTest.java
@@ -4,7 +4,8 @@ package info.freelibrary.solr;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.xml.XmlPage;
+
 import com.gargoylesoftware.htmlunit.WebClient;
 
 import org.slf4j.Logger;
@@ -18,18 +19,22 @@ public class ISO639SolrIntegrationTest {
 
     private static final String SELECT = "/select/?q=*:*&indent=on";
 
+    private static final String FACET = "&facet=true&facet.field=iso639";
+
     private static final Logger LOGGER = LoggerFactory
             .getLogger(ISO639SolrIntegrationTest.class);
 
     @Test
     public void test() {
-        WebClient webClient = new WebClient();
+        WebClient client = new WebClient();
 
         try {
-            HtmlPage page = webClient.getPage(URL + SELECT);
+            XmlPage page = client.getPage(URL + SELECT + FACET);
             LOGGER.info(page.asXml());
         } catch (Exception details) {
             fail(details.getMessage());
+        } finally {
+            client.closeAllWindows();
         }
     }
 
