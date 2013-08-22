f0221f1229b0ebe0894e76ad0dd96e7ba6bebb47
Kevin S. Clarke <ksclarke@gmail.com>
2013-08-22 10:13:23 -0400

    reworked how solr api compatibility is selected [skip ci]

---
+++ src/solr-4.2/java/info/freelibrary/solr/ISO639ConversionFilterFactory.java
@@ -0,0 +1,24 @@
+package info.freelibrary.solr;
+
+import java.util.Map;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.util.TokenFilterFactory;
+
+/**
+ * TokenFilterFactory that returns an ISO639ConversionFilter for Solr to use.
+ * 
+ * @author Kevin S. Clarke
+ */
+public class ISO639ConversionFilterFactory extends TokenFilterFactory {
+    
+    public ISO639ConversionFilterFactory() {
+        super();
+    }
+
+    @Override
+    public ISO639ConversionFilter create(TokenStream aTokenStream) {
+        return new ISO639ConversionFilter(aTokenStream);
+    }
+
+}
