c562fbea7ba604109d14b81614da891cde58e832
Kevin S. Clarke <ksclarke@gmail.com>
2015-06-15 22:01:18 -0400

    Update version in release script

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@
 
 # The current solr-iso639-filter release version.  Release version numbers use
 # the form of rYYYYMMDD; they represent code changes in the filter itself...
-RELEASE_VERSION="r20131208"
+RELEASE_VERSION="r20150615"
 
 # The versions of Solr that are supported by this script
 SOLR_VERSIONS="3.6.1 3.6.2 4.0.0 4.1.0 4.2.0 4.2.1 4.3.0 4.3.1 4.4.0 4.5.0 4.5.1 4.6.0 4.6.1 4.7.0 4.7.1 4.7.2 4.8.0 4.8.1 4.9.0 4.9.1 4.10.0 4.10.1 4.10.2 4.10.3 4.10.4"
